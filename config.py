d0d99aa79db4693056ec4911aa85a3bfeb62b70f
NarayanAdithya <narayanadithya1234@gmail.com>
2020-11-29 00:22:45 +0530

    Added DB and Login Config

--- config.py
+++ config.py
@@ -3,4 +3,6 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 
 class config(object):
     SECRET_KEY= os.environ.get('SECRET_KEY') or '334nadnj&&89Ydau89YAd98adbszmdi3*&&923kln'
-    
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \
+        'sqlite:///'+os.path.join(basedir,'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
