786f7b9cd0db6a3feee97d178fb749e5bf2f883c
NarayanAdithya <narayanadithya1234@gmail.com>
2020-11-29 00:53:06 +0530

    Major DB Change

--- app/models.py
+++ app/models.py
@@ -12,6 +12,10 @@ def load_user(id):
     return User.query.get(int(id))
 
 
+enrolled=db.Table('Enrolled',
+    db.Column('user_id',db.Integer,db.ForeignKey('user.id')),
+    db.Column('course_id',db.Integer,db.ForeignKey('courses.id'))
+)
 
 class User(UserMixin,db.Model):
     id=db.Column(db.Integer,primary_key=True)
@@ -20,13 +24,15 @@ class User(UserMixin,db.Model):
     user_role=db.Column(db.String(20))
     Region=db.Column(db.String(20))
     password_hash=db.Column(db.String(128))
+    threads=db.relationship('thread',backref='creator',lazy='dynamic')
+    posts=db.relationship('post',backref='Author',lazy='dynamic')
     last_seen=db.Column(db.DateTime,default=datetime.utcnow)
     twitter=db.Column(db.String(120),default="N/A")
     facebook=db.Column(db.String(120),default="N/A")
     instagram=db.Column(db.String(120),default="N/A")
     birthdate=db.Column(db.String(120),default="N/A")
     Interests=db.Column(db.String(200),default="N/A")
-    
+    provides_course=db.relationship('Courses',backref="Teacher",lazy='dynamic')
     def __repr__(self):
         return '<Role:{} Name:{} Id:{}>'.format(self.user_role,self.username,self.id)
     def set_password(self,password):
@@ -65,3 +71,26 @@ class post(db.Model):
     thread_id=db.Column(db.Integer,db.ForeignKey('thread.id'))
     def __repr__(self):
         return '<Post in thread {} by {}>'.format(self.thread_id,self.user_id)
+
+class Courses(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    course_code=db.Column(db.String(10),unique=True)
+    Course_name=db.Column(db.String(100))
+    Course_Description=db.Column(db.String(250))
+    resources_link=db.Column(db.String(250))
+    created=db.Column(db.DateTime,default=datetime.utcnow)
+    Instructor_id=db.Column(db.Integer,db.ForeignKey('user.id'))
+    students_enrolled=db.relationship('User',secondary=enrolled,backref="Courses_enrolled",lazy='dynamic')
+    def __repr__(self):
+        return '<Course {} made by {}>'.format(self.course_code,self.Instructor_id)
+    def add_student(self, user):
+        if not self.is_student(user):
+            self.students_enrolled.append(user)
+
+    def remove_student(self, user):
+        if self.is_student(user):
+            self.students_enrolled.remove(user)
+
+    def is_student(self, user):
+        return self.students_enrolled.filter(
+            enrolled.c.user_id == user.id).count() > 0
--- celis_app.py
+++ celis_app.py
@@ -1,11 +1,11 @@
 from app import app,db,socketio
-from app.models import User
+from app.models import User,thread,post,Courses,enrolled
 
 
 # Flask Shell Command Gives Access to DataBase Manipulation Facilated Through Flask SQLAlchemy
 @app.shell_context_processor
 def make_shell_context():
-    return {'db':db,'User':User}
+    return {'db':db,'User':User,'thread':thread,'post':post,'course':Courses}
 
 
 if __name__ == '__main__':
