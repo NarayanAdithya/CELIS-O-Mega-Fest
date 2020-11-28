e9c85080233fafebb0282933a1cb7ba210d9c86c
NarayanAdithya <narayanadithya1234@gmail.com>
2020-11-28 22:53:57 +0530

    added html

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,4 @@ from flask import request,redirect,url_for,render_template,flash,get_flashed_mes
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('celis.html')
+    return render_template('celis.html',title='Home',data_footer_aos="fade-left",data_aos_footer_delay=100,data_aos_header="fade-left",data_header_aos_delay=100)
