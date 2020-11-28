3ad889679ced5dbe4cd9a11ac7f24a2b968adae7
NarayanAdithya <narayanadithya1234@gmail.com>
2020-11-29 00:23:48 +0530

    Added Shell Interface Initialisation for DB manipulation

--- celis_app.py
+++ celis_app.py
@@ -1,4 +1,12 @@
-from app import app,socketio
+from app import app,db,socketio
+from app.models import User,thread,post,Courses,enrolled
+
+
+# Flask Shell Command Gives Access to DataBase Manipulation Facilated Through Flask SQLAlchemy
+@app.shell_context_processor
+def make_shell_context():
+    return {'db':db,'User':User}
+
 
 if __name__ == '__main__':
-    socketio.run(app,debug=True,log_output=True,)
+    socketio.run(app,log_output=True,)
