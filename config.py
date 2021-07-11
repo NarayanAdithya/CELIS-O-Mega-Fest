5cb2745d559c18d4ed1162d2cff5261331f39483
NarayanAdithya <narayanadithya1234@gmail.com>
2021-07-11 12:13:08 +0530

    Fixing Environ Issue

--- config.py
+++ config.py
@@ -6,3 +6,9 @@ class config(object):
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \
         'sqlite:///'+os.path.join(basedir,'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+    MAIL_SERVER=os.environ.get('MAIL_SERVER')
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
+    MAIL_USE_TLS=os.environ.get('MAIL_USE_TLS')
+    MAIL_PORT=os.environ.get('MAIL_PORT')
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+    ADMINS=['aravindhari11@gmail.com']
